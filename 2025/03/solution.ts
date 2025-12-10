8a378f02c58859763aa8c54630f7f49d06b86e57
zain <dev.zainajmal@gmail.com>
2025-12-10 19:16:07 +0500

    pb3 sol 1

--- 2025/03/solution.ts
+++ 2025/03/solution.ts
@@ -3,7 +3,7 @@ import fs from "fs/promises";
 
 async function readFileContent(filePath: string): Promise<string[]> {
   const rawData = await fs.readFile(filePath, "utf-8");
-  return rawData.split(",");
+  return rawData.split("\n");
 }
 
 function isInvalidId(id: number): boolean {
@@ -20,13 +20,27 @@ function checkwithRegex(id: number): boolean {
 }
 
 function stepOne(Id: string[]): number {
+  console.log(Id);
   let password: number[] = [];
   Id.forEach((element) => {
-    const [start, end] = element.split("-").map(Number);
-    console.log(start, end);
-    for (let i = start!; i <= end!; i++) {
-      if (isInvalidId(i)) password.push(i);
+    const serial = element.split("");
+    let largest = 0;
+    let secondLargest = 0;
+    let largestIndex = 0;
+    let secondLargestIndex = 0;
+    for (let i = 0; i < serial.length; i++) {
+      if (Number(serial[i]) > largest && i !== serial.length - 1) {
+        largest = Number(serial[i]);
+        largestIndex = i;
+      }
     }
+    for (let j = largestIndex+1; j < serial.length; j++) {
+      if (Number(serial[j]) > secondLargest) {
+        secondLargest = Number(serial[j]);
+        secondLargestIndex = j;
+      }
+    }
+    password.push(Number(largest.toString() + secondLargest.toString()));
   });
   console.log(password);
   return password.reduce((a, b) => a + b);
@@ -45,7 +59,7 @@ function stepTwo(Id: string[]): number {
   return password.reduce((a, b) => a + b);
 }
 
-const input = await readFileContent("./sample.txt");
+const input = await readFileContent("./input.txt");
 const result = stepOne(input);
 // const result = stepTwo(input);
 console.log("Result:", result);
