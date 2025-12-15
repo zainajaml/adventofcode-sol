1384e82307809809985d414ee6bb35f1b29ca16f
zain <dev.zainajmal@gmail.com>
2025-12-15 17:36:15 +0500

    pb-3 sol part 2

--- 2025/03/solution.ts
+++ 2025/03/solution.ts
@@ -6,19 +6,6 @@ async function readFileContent(filePath: string): Promise<string[]> {
   return rawData.split("\n");
 }
 
-function isInvalidId(id: number): boolean {
-  let idStr = id.toString();
-  const firsthalf = idStr.slice(0, Math.floor(idStr.length / 2));
-  const secondhalf = idStr.slice(Math.floor(idStr.length / 2));
-  return firsthalf === secondhalf;
-}
-
-function checkwithRegex(id: number): boolean {
-  const idStr = id.toString();
-  const regex = /^(\d+)\1+$/;
-  return regex.test(idStr);
-}
-
 function stepOne(Id: string[]): number {
   console.log(Id);
   let password: number[] = [];
@@ -34,7 +21,7 @@ function stepOne(Id: string[]): number {
         largestIndex = i;
       }
     }
-    for (let j = largestIndex+1; j < serial.length; j++) {
+    for (let j = largestIndex + 1; j < serial.length; j++) {
       if (Number(serial[j]) > secondLargest) {
         secondLargest = Number(serial[j]);
         secondLargestIndex = j;
@@ -48,18 +35,28 @@ function stepOne(Id: string[]): number {
 
 function stepTwo(Id: string[]): number {
   let password: number[] = [];
+  console.log(Id);
   Id.forEach((element) => {
-    const [start, end] = element.split("-").map(Number);
-    console.log(start, end);
-    for (let i = start!; i <= end!; i++) {
-      if (checkwithRegex(i)) password.push(i);
+    const serial = element.split("");
+    const digits: number[] = [];
+    let currentMaxIndex= 0;
+    for (let need = 11; need >= 0 ; need--) {
+      for (let i = currentMaxIndex; i < serial.length - need; i++) {
+        if (Number(serial[i]) > Number(serial[currentMaxIndex])   ) {
+          currentMaxIndex = i;
+        }
+      }
+      digits.push(Number(serial[currentMaxIndex]));
+      currentMaxIndex = currentMaxIndex + 1;
     }
+    password.push(Number(digits.join("")));
   });
+
   console.log(password);
   return password.reduce((a, b) => a + b);
 }
 
 const input = await readFileContent("./input.txt");
-const result = stepOne(input);
-// const result = stepTwo(input);
+// const result = stepOne(input);
+const result = stepTwo(input);
 console.log("Result:", result);
